1230450cc57c2eefa4cac938feaeb7f68bbb2135
Scott Salisbury <scottsalisbury@gmail.com>
2017-02-26 15:50:34 +0000

    Updates

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var chalk = require('chalk');
 
-console.log('To use this script, make sure that the following script is added to your package.json scripts block...\n');
+console.log('To use this script make sure that the following is added to your package.json scripts block...\n');
 console.log(chalk.cyan('"scripts": {\n  "setup-deploy":"setup-deploy-config"\n}'));
 console.log('\nThen run ' + chalk.cyan('npm run setup-deploy') + ' to start the setup.\n');
