58f2155b68176fb1563910ce004533c598ac2636
Scott Salisbury <scottsalisbury@gmail.com>
2017-02-26 15:23:28 +0000

    Updates

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 
-console.log('To use this script, please make sure\n"setup-deploy": "setup-deploy-config"\nis added to your package.json scripts block.');
+console.log('To use this script, please make sure "setup-deploy": "setup-deploy-config" is added to your package.json scripts block.\n');
